1a2df7617bc65f0ac2f25e85f0aeeb3d7ffe0954
Leonid Semenov <ln.sm.study@gmail.com>
2021-05-03 20:39:40 +0300

    Added the ability to autofill forms with password managers.

--- lib/components/loginForm.dart
+++ lib/components/loginForm.dart
@@ -50,142 +50,150 @@ class LoginCard extends StatelessWidget {
               Center(
                 child: Form(
                     key: _loginFormKey,
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 80,
-                          child: Center(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 30, right: 30),
-                              child: TextFormField(
-                                style: TextStyle(color: Colors.white),
-                                validator: (value) {
-                                  if (value!.trim().isEmpty) {
-                                    return 'Please enter some text';
-                                  }
-                                  return null;
-                                },
-                                controller: loginController,
-                                decoration: InputDecoration(
-                                    border: UnderlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(15.0)),
-                                    icon: Icon(Icons.person),
-                                    hintText: 'Login',
-                                    filled: true,
-                                    fillColor: inputColor),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          height: 80,
-                          child: Center(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 30, right: 30),
-                              child: TextFormField(
-                                style: TextStyle(color: Colors.white),
-                                obscureText: true,
-                                validator: (value) {
-                                  if (value!.trim().isEmpty) {
-                                    return 'Please enter some text';
-                                  }
-                                  return null;
-                                },
-                                controller: passwordController,
-                                decoration: InputDecoration(
-                                    border: UnderlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(15.0)),
-                                    icon: Icon(Icons.lock),
-                                    hintText: 'Password',
-                                    filled: true,
-                                    fillColor: inputColor),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                top: 10, left: 30, bottom: 10, right: 30),
+                    child: AutofillGroup(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 80,
                             child: Center(
-                              child: ElevatedButton(
-                                style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(18.0),
-                                ))),
-                                onPressed: () {
-                                  if (_loginFormKey.currentState!.validate()) {
-                                    authenticate(
-                                      loginController.text,
-                                      passwordController.text,
-                                    )
-                                        .then((data) => {writeUser(data)})
-                                        .catchError((error) => {
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                                SnackBar(
-                                                  content:
-                                                      Text(error.toString()),
-                                                  duration: Duration(
-                                                      milliseconds: 1500),
-                                                  width:
-                                                      280.0, // Width of the SnackBar.
-                                                  padding: EdgeInsets.symmetric(
-                                                      horizontal:
-                                                          8.0), // Inner padding for SnackBar content.
-                                                  behavior:
-                                                      SnackBarBehavior.floating,
-                                                  shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            10.0),
-                                                  ),
-                                                ),
-                                              )
-                                            });
-                                  }
-                                },
-                                child: const Text('Log in'),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                child: Center(
-                                  child: Text(
-                                    "Don't have an account?",
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 30, right: 30),
+                                child: TextFormField(
+                                  autofillHints: <String>[AutofillHints.email],
+                                  style: TextStyle(color: Colors.white),
+                                  validator: (value) {
+                                    if (value!.trim().isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                  controller: loginController,
+                                  decoration: InputDecoration(
+                                      border: UnderlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15.0)),
+                                      icon: Icon(Icons.person),
+                                      hintText: 'Login',
+                                      filled: true,
+                                      fillColor: inputColor),
                                 ),
                               ),
-                              Container(
-                                child: Center(
-                                  child: TextButton(
-                                    onPressed: () {
-                                      loginController.text = '';
-                                      passwordController.text = '';
-                                      enterStateSetter();
-                                    },
+                            ),
+                          ),
+                          Container(
+                            height: 80,
+                            child: Center(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 30, right: 30),
+                                child: TextFormField(
+                                  autofillHints: <String>[
+                                    AutofillHints.password
+                                  ],
+                                  style: TextStyle(color: Colors.white),
+                                  obscureText: true,
+                                  validator: (value) {
+                                    if (value!.trim().isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                  controller: passwordController,
+                                  decoration: InputDecoration(
+                                      border: UnderlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15.0)),
+                                      icon: Icon(Icons.lock),
+                                      hintText: 'Password',
+                                      filled: true,
+                                      fillColor: inputColor),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  top: 10, left: 30, bottom: 10, right: 30),
+                              child: Center(
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ))),
+                                  onPressed: () {
+                                    if (_loginFormKey.currentState!
+                                        .validate()) {
+                                      authenticate(
+                                        loginController.text,
+                                        passwordController.text,
+                                      )
+                                          .then((data) => {writeUser(data)})
+                                          .catchError((error) => {
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  SnackBar(
+                                                    content:
+                                                        Text(error.toString()),
+                                                    duration: Duration(
+                                                        milliseconds: 1500),
+                                                    width:
+                                                        280.0, // Width of the SnackBar.
+                                                    padding: EdgeInsets.symmetric(
+                                                        horizontal:
+                                                            8.0), // Inner padding for SnackBar content.
+                                                    behavior: SnackBarBehavior
+                                                        .floating,
+                                                    shape:
+                                                        RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              10.0),
+                                                    ),
+                                                  ),
+                                                )
+                                              });
+                                    }
+                                  },
+                                  child: const Text('Log in'),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Center(
                                     child: Text(
-                                      "Sign up",
-                                      style:
-                                          TextStyle(color: Colors.blueAccent),
+                                      "Don't have an account?",
+                                      style: TextStyle(color: Colors.grey),
                                     ),
                                   ),
                                 ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                                Container(
+                                  child: Center(
+                                    child: TextButton(
+                                      onPressed: () {
+                                        loginController.text = '';
+                                        passwordController.text = '';
+                                        enterStateSetter();
+                                      },
+                                      child: Text(
+                                        "Sign up",
+                                        style:
+                                            TextStyle(color: Colors.blueAccent),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     )),
               )
             ],
